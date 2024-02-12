99b0795ef9626fc73477c107251e6c36de59a283
Kaue Evaristo <159561976+evaristo1kaue@users.noreply.github.com>
2024-02-12 10:27:38 -0300

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,10 @@
+package com.mycompany.userregistration.simple;
+class Simple{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+        
+    }
+}
