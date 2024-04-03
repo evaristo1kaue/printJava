658dc360d473dcfb59598546eed19d982080a2ae
Kaue Evaristo <159561976+evaristo1kaue@users.noreply.github.com>
2024-04-03 15:25:43 -0300

    Update and rename mathematicsOperations to insertValuebles.java

---
+++ insertValuebles.java
@@ -0,0 +1,33 @@
+//Importando uma biblioteca para usar entrada de dados
+import java.util.Scanner;
+
+//Abrindo uma classe obrgatória na linguagem Java
+public class CalculoMedia{
+  
+  public static void main(String[] args){
+    //Criando um objeto da classe
+    Scanner scanner = new Scanner(System.in);
+
+    //Informativo de cabeçalho
+    system.out.println("--Calculadora de média de 3 números--");
+
+    //Primeiro número
+    system.out.print("Digite o primeiro número: ");
+    double num1 = scanner.nextDouble();
+
+    //Segundo número
+    system.out.print("Digite o segundo número: ");
+    double num2 = scanner.nextDouble();
+
+    //Terceiro número
+    system.out.print("Digite o terceiro número: ");
+    double num3 = scanner.nextDouble();
+
+    double media = (num1+num2+num3)/3;
+
+    system.out.print("Esse é a média dos 3 números: "+media);
+
+    scanner.close();
+  }
+
+}
